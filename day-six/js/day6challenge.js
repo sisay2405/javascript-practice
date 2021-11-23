6738e05504a7ed304c89add852321845cbfaaaca
sisay2405 <82465149+sisay2405@users.noreply.github.com>
2021-11-22 16:12:28 -0800

    update

--- day-six/js/day6challenge.js
+++ day-six/js/day6challenge.js
@@ -1,14 +1,40 @@
-function CatFacts() {
-      fetch("https://cat-fact.herokuapp.com/facts")
-        .then((res) => {
-          return res.json();
-        })
-        .then(function (facts) {
-          const factsLength = facts.length;
-          const randomNumber = Math.random() * factsLength;
-          const flooredNumber = Math.floor(randomNumber);
-          alert(facts[flooredNumber].text);
-        });
-    }
+// first method 
+// function CatFacts() {
+//       fetch("https://cat-fact.herokuapp.com/facts")
+//         .then((res) => {
+//           return res.json();
+//         })
+//         .then(function (facts) {
+//           const factsLength = facts.length;
+//           const randomNumber = Math.random() * factsLength;
+//           const flooredNumber = Math.floor(randomNumber);
+//           alert(facts[flooredNumber].text);
+//         });
+//     }
     
-    CatFacts();
+//     CatFacts();
+//   second method 
+    function fetchFacts() {
+		fetch(
+			`https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1`
+		)
+            .then((response) => response.json())
+			.then((data) => alert(data.text)
+        );        
+        }
+
+        fetchFacts()
+//  third method 
+        // function fetchFacts() {
+        //     fetch(
+        //         `https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1`
+        //     )
+        //         .then((response) => response.text())
+        //         .then((data) =>  
+        //         {
+        //         	let fact = JSON.parse(data).text;
+        //         	alert(fact);
+        //         });
+        //         }
+    
+        //     fetchFacts()
